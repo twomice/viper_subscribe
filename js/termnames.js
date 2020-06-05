77279837293426f472e3a1351b8e62b7efc0f23b
Eli Lisseck <lisseck@gmail.com>
2020-06-05 09:25:40 -0700

    add variable label for content type

--- js/termnames.js
+++ js/termnames.js
@@ -6,6 +6,9 @@
         var str = $('.js-flag-subscribe-term-' + index + ' a').text().replace('[viper_subscribe:term_name]', value);
         $('.js-flag-subscribe-term-' + index + ' a').text(str);
       });
+      //change the content type flag
+      var str2 = $('.flag-subscribe-content-type a').text().replace('[viper_subscribe:bundle]', drupalSettings.viper_subscribe.bundle);
+      $('.flag-subscribe-content-type a').text(str2);
     }
   };
 })(jQuery, Drupal);
