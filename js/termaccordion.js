6b1d49789cbc77fb285b0d71472e5ac97bd8da9b
Allen Shaw <allen@JoineryHQ.com>
2020-08-06 17:40:17 -0500

    Use cursor:pointer for accordions

--- js/termaccordion.js
+++ js/termaccordion.js
@@ -7,7 +7,7 @@
       $('.flag-subscribe-term').hide();
           if ($('.flag-accordion').length < 1) {
             $('.flag-subscribe-content-type').add('.flag-subscribe-node').add('.flag-subscribe-term').add('.flag-subscribe-user').wrapAll('<div class="flag-accordion"></div>');
-            $('<div id="flag-toggle"><h3>Click for Subscription Options</h3></div>').insertBefore('.flag-accordion');
+            $('<div id="flag-toggle" style="cursor:pointer"><h3>Click for Subscription Options</h3></div>').insertBefore('.flag-accordion');
             $('#flag-toggle').click(function() {
             if($('.flag-subscribe-content-type').is(":visible")) {
               $('.flag-subscribe-content-type').hide('slow');
